0dfebce39b445f5089fbc82082bda1e9a0dbca5d
vasani05 <akash.vmc13@sot.pdpu.ac.in>
2023-03-28 20:00:34 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("adding all ml related  code here")
+
+def model1():
+    print("adding code for model no. 1")
+
+def model2():
+    print("adding code for model no. 1")
+
+def model3():
+    print("adding code for model no. 1")
